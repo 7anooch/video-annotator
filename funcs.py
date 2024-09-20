6d73ba29be1d58f27b2b63fda63bba4d2f727665
7anooch <129010989+7anooch@users.noreply.github.com>
2024-09-20 16:29:53 -0700

    moved a function to a different script

--- funcs.py
+++ funcs.py
@@ -31,3 +31,29 @@ def get_csv_file_path(video_path, output_csv_name=None):
     csv_file_path = os.path.join(video_dir, csv_file_name)
     
     return csv_file_path
+
+def get_longest_common_substring(strs):
+    if not strs:
+        return ""
+    
+    shortest_str = min(strs, key=len)
+    
+    def is_common_substring(length):
+        for i in range(len(shortest_str) - length + 1):
+            substr = shortest_str[i:i + length]
+            if all(substr in s for s in strs):
+                return substr
+        return None
+    
+    low, high = 0, len(shortest_str)
+    result = ""
+    while low <= high:
+        mid = (low + high) // 2
+        substr = is_common_substring(mid)
+        if substr:
+            result = substr
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    return result
--- gen_ground_truth.py
+++ gen_ground_truth.py
@@ -7,6 +7,8 @@ import tkinter as tk
 from tkinter import filedialog
 import csv
 
+from funcs import get_longest_common_substring
+
 def load_annotations(csv_path):
     annotations = {}
     if os.path.exists(csv_path):
@@ -17,31 +19,6 @@ def load_annotations(csv_path):
         print(f"No annotation file found at {csv_path}")
     return annotations
 
-def get_longest_common_substring(strs):
-    if not strs:
-        return ""
-    
-    shortest_str = min(strs, key=len)
-    
-    def is_common_substring(length):
-        for i in range(len(shortest_str) - length + 1):
-            substr = shortest_str[i:i + length]
-            if all(substr in s for s in strs):
-                return substr
-        return None
-    
-    low, high = 0, len(shortest_str)
-    result = ""
-    while low <= high:
-        mid = (low + high) // 2
-        substr = is_common_substring(mid)
-        if substr:
-            result = substr
-            low = mid + 1
-        else:
-            high = mid - 1
-    
-    return result
 
 def get_common_basename_prefix(csv_paths):
     basenames = [os.path.basename(path.split('.csv')[0]) for path in csv_paths]
