dfab5c94fcf9ab1f20cc38744588c8be5629ab8a
7anooch <129010989+7anooch@users.noreply.github.com>
2025-02-27 14:31:15 -0800

    fill in small segments

--- patch_gaps.py
+++ patch_gaps.py
@@ -2,7 +2,9 @@ import os
 import pandas as pd
 import argparse
 import tkinter as tk
+import numpy as np
 from tkinter import filedialog
+from scipy.ndimage import label
 
 def load_annotations(csv_path):
     annotations = {}
@@ -38,6 +40,71 @@ def get_all_annotations(csv_paths):
 
     return all_annotations, min_length
 
+def connected_components_1D(arr, dust_size=None):
+    lbl, n_features = label(arr)
+    indices = np.indices(lbl.shape).T[:, 0]
+    cc_dict = {}
+    for i in range(1, n_features + 1):
+        cc_i = indices[lbl == i]
+        if dust_size is not None:
+            if np.shape(cc_i)[0] < dust_size:
+                continue
+
+        cc_dict[i] = cc_i
+
+    return cc_dict
+
+def analyze_sequence(annotations):
+    if not annotations:
+        return []
+
+    sorted_frames = sorted(annotations.keys())
+    current_label = annotations[sorted_frames[0]]
+    start_frame = sorted_frames[0]
+    sequence_summary = []
+    frame_list = []
+
+    for frame in sorted_frames[1:]:
+        label = annotations[frame]
+        if label != current_label:
+            duration = frame - start_frame
+            frame_list.append(frame)
+            sequence_summary.append((current_label, duration, frame_list))
+            current_label = label
+            start_frame = frame
+            frame_list = []
+        frame_list.append(frame)
+
+    duration = sorted_frames[-1] - start_frame + 1
+    sequence_summary.append((current_label, duration, frame_list))
+
+    return sequence_summary
+
+def find_gaps(updated_ground_truth_dict, max_gap_size=30):
+    components = analyze_sequence(updated_ground_truth_dict)
+    label_cc = {}
+    for i, (lbl, dur, fr) in enumerate(components):
+        if lbl not in label_cc:
+            label_cc[lbl] = []
+        label_cc[lbl].append([i, dur, fr])
+
+    gaps = []
+    for l in label_cc.keys():
+        ind_diff  = np.diff([x[0] for x in label_cc[l]])
+        diff2 = np.where(ind_diff == 2)[0]
+        for d in diff2:
+            gap_start = label_cc[l][d][2][-1]
+            gap_end = label_cc[l][d+1][2][0]
+            gap_frames = list(range(gap_start, gap_end+1))
+            gap_size = len(gap_frames)
+            if gap_size < max_gap_size:
+                gaps.append((l, gap_size, gap_frames))
+                print(f'set segment of length {gap_size} to label:',  l)
+                print(gap_frames)
+
+    gaps.sort(key=lambda x: x[1])
+    return gaps
+
 def fill_missing_labels(ground_truth_path, ground_truth_ds_path, output_path):
     ground_truth = load_annotations(ground_truth_path)
     ground_truth_ds = load_annotations(ground_truth_ds_path)
@@ -48,6 +115,22 @@ def fill_missing_labels(ground_truth_path, ground_truth_ds_path, output_path):
 
     updated_ground_truth_list = [{'frame': frame, 'label': int(label)} 
                                  for frame, label in sorted(ground_truth.items())]
+    updated_ground_truth_dict = {entry['frame']: entry['label'] 
+                                 for entry in updated_ground_truth_list}
+    
+    gaps = find_gaps(updated_ground_truth_dict)
+
+    while gaps != []:
+        for l, gap_size, gap_frames in gaps:
+            for entry in updated_ground_truth_list:
+                if entry['frame'] in gap_frames:
+                    entry['label'] = l
+
+            updated_ground_truth_dict = {ent['frame']: ent['label'] 
+                                        for ent in updated_ground_truth_list}
+
+            gaps = find_gaps(updated_ground_truth_dict)
+
     df_updated_gt = pd.DataFrame(updated_ground_truth_list)
     df_updated_gt.to_csv(output_path, index=False)
     print(f"\nUpdated ground truth saved to {output_path}")
